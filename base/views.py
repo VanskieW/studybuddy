407c7686482321e27972908c2a7fee63039a2da6
VanskieW <evanswandz@gmail.com>
2022-02-09 12:58:29 +0300

    Study Buddy 1st draft

---
+++ base/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Room
+
+rooms = [
+    {"id": 1, "name": "Python"},
+    {"id": 2, "name": "Javascript"},
+    {"id": 3, "name": "React"},
+]
+
+def home(request):
+
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render (request, 'base/home.html', context)
+
+def room(request, pk):
+
+    room = None
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render (request, 'base/rooms.html', context)
+
+# Create your views here
